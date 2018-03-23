2a743762dc4a3504855cd59358a720f5d5142baa
Stareat0707 <stareat0707@gmail.com>
2018-03-23 11:02:50 +0900

    Solve question 6.

--- 20180316_6/main.cpp
+++ 20180316_6/main.cpp
@@ -2,6 +2,48 @@
 #include <stack>
 #include <string>
 
+bool function(std::stack<int>& stack, int num)
+{
+	if (stack.empty())
+	{
+		std::cout << -1 << std::endl;
+		return false;
+	}
+
+	if (stack.top() == '(' || stack.top() == '[')
+	{
+		stack.pop();
+		int x = num;
+		if (!stack.empty() && stack.top() != '(' && stack.top() != '[')
+		{
+			int y = stack.top();
+			stack.pop();
+			x += y;
+		}
+		stack.push(x);
+	}
+	else if (stack.size() >= num && stack.top() != '(' && stack.top() != '[')
+	{
+		int x = stack.top() * num;
+		stack.pop();
+		stack.pop();
+		if (!stack.empty() && stack.top() != '(' && stack.top() != '[')
+		{
+			int y = stack.top();
+			stack.pop();
+			x += y;
+		}
+		stack.push(x);
+	}
+	else
+	{
+		std::cout << -1 << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
 int main()
 {
 	std::stack<int> stack;
@@ -12,49 +54,21 @@ int main()
 	for (int i = 0; i < input.size(); ++i)
 	{
 		char c = input.at(i);
+		int x;
 		switch (c)
 		{
 		case '(':
+		case '[':
 			stack.push(c);
 			break;
 		case ')':
-			if (stack.empty())
-			{
-				std::cout << -1 << std::endl;
+			if (!function(stack, 2))
 				return 0;
-			}
-
-			int x = 0;
-			if (stack.top() == '(')
-			{
-				stack.pop();
-				x = 3;
-				if (!stack.empty() && stack.top() != '(')
-				{
-					int y = stack.top();
-					stack.pop();
-					x += y;
-				}
-				stack.push(x);
-			}
-			else if (stack.size() >= 3 && stack.top() != '(')
-			{
-				x = stack.top() * 3;
-				stack.pop();
-				stack.pop();
-				if (!stack.empty() && stack.top() != '(')
-				{
-					int y = stack.top();
-					stack.pop();
-					x += y;
-				}
-				stack.push(x);
-			}
-			else
-			{
-				std::cout << -1 << std::endl;
+			break;
+		case ']':
+			if (!function(stack, 3))
 				return 0;
-			}
+			break;
 		}
 	}
 	std::cout << stack.top() << std::endl;
